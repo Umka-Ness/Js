81c7e0a6f9175cb4a4843bf7cb834570fb86644a
Umka_Ness <melnik.dmitri2012@gmail.com>
2023-02-02 06:45:04 +0100

    update all

--- index.js
+++ index.js
@@ -1076,14 +1076,10 @@ timeUpdate()
 
 function timeUpdate() {
     const newDate = new Date(`Jan 1, ${new Date().getFullYear() +1}`)
-    console.log(newDate)
 
     const neew = new Date()
-    console.log(neew)
 
     const diff = newDate - neew
-    console.log(diff
-        )
     const days = Math.floor(diff / (1000 * 60 * 60 * 24))
     const hours = Math.floor(diff / (1000 * 60 * 60 ) % 24)
     const minutes = Math.floor(diff / (1000 * 60) % 60)
@@ -1102,29 +1098,39 @@ startTimer.addEventListener("click", e => {
     startTimer.disabled = true
     intervalId = setInterval(timeUpdate,1000)
     countTap += 1
+    console.log("countTap = " + countTap + "startTimer")
+    console.log(intervalId + "intervalId")
 })
 stopTimer.addEventListener("click", e =>{
     stopAndStart.disabled = true
     stopTimer.disabled = true
     startTimer.disabled = false
-    clearInterval(intervalId)
     countTap += 1
+    clearInterval(intervalId)
+    console.log("countTap = " + countTap + "stopTimer")
+    console.log(intervalId + "intervalId")
+
 })
 
 stopAndStart.addEventListener("click", oneClickFun)
 
 function oneClickFun() {
     if(countTap % 2 === 0) {
-        
         clearInterval(intervalId)
         stopAndStart.id = "start"
         stopAndStart.disabled = false
         countTap += 1
-        console.log(12312)
+        stopTimer.disabled = true
+        startTimer.disabled = false
+        console.log("countTap = " + countTap + "if")
     }
     else {
         intervalId = setInterval(timeUpdate,1000)
         stopAndStart.id = "stopAndStart"
         countTap += 1
+        console.log("countTap = " + countTap + "else")
+        console.log(intervalId + "else")
+        startTimer.disabled = true
+        stopTimer.disabled = false
     }
 }
