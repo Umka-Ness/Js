8043a8ac8977a6e9b1b5f0225ad861161b1819ff
Umka_Ness <melnik.dmitri2012@gmail.com>
2023-02-04 04:18:40 +0100

    update Promise

--- index.js
+++ index.js
@@ -14,6 +14,7 @@
 // console.log(array.addPoint(100))
 // console.log(array)
 
+
 // Constructor down
  
 // let dima = new constructor("Dima", 23)
@@ -1066,71 +1067,103 @@
 //     console.log("onReject " + error)
 // } ) } )
 
-const timer = document.getElementById("timerr")
-const stopTimer = document.getElementById("stop")
-const startTimer = document.getElementById("start")
-const stopAndStart = document.getElementById("stopAndStart")
-let countTap = 0
+// const timer = document.getElementById("timerr")
+// const stopTimer = document.getElementById("stop")
+// const startTimer = document.getElementById("start")
+// const stopAndStart = document.getElementById("stopAndStart")
+// let countTap = 0
 
-timeUpdate()
+// timeUpdate()
 
-function timeUpdate() {
-    const newDate = new Date(`Jan 1, ${new Date().getFullYear() +1}`)
+// function timeUpdate() {
+//     const newDate = new Date(`Jan 1, ${new Date().getFullYear() +1}`)
 
-    const neew = new Date()
+//     const neew = new Date()
 
-    const diff = newDate - neew
-    const days = Math.floor(diff / (1000 * 60 * 60 * 24))
-    const hours = Math.floor(diff / (1000 * 60 * 60 ) % 24)
-    const minutes = Math.floor(diff / (1000 * 60) % 60)
-    const sec = Math.floor((diff / 1000) % 60)
+//     const diff = newDate - neew
+//     const days = Math.floor(diff / (1000 * 60 * 60 * 24))
+//     const hours = Math.floor(diff / (1000 * 60 * 60 ) % 24)
+//     const minutes = Math.floor(diff / (1000 * 60) % 60)
+//     const sec = Math.floor((diff / 1000) % 60)
 
-    timer.textContent = `${days} d. ${hours} h. ${minutes} minutes. ${sec} sec.`
+//     timer.textContent = `${days} d. ${hours} h. ${minutes} minutes. ${sec} sec.`
+// }
+
+// let intervalId = setInterval(timeUpdate,1000)
+
+
+
+// startTimer.addEventListener("click", e => {
+//     stopAndStart.disabled = false
+//     stopTimer.disabled = false
+//     startTimer.disabled = true
+//     intervalId = setInterval(timeUpdate,1000)
+//     countTap += 1
+//     console.log("countTap = " + countTap + "startTimer")
+//     console.log(intervalId + "intervalId")
+// })
+// stopTimer.addEventListener("click", e =>{
+//     stopAndStart.disabled = true
+//     stopTimer.disabled = true
+//     startTimer.disabled = false
+//     countTap += 1
+//     clearInterval(intervalId)
+//     console.log("countTap = " + countTap + "stopTimer")
+//     console.log(intervalId + "intervalId")
+
+// })
+
+// stopAndStart.addEventListener("click", oneClickFun)
+
+// function oneClickFun() {
+//     if(countTap % 2 === 0) {
+//         clearInterval(intervalId)
+//         stopAndStart.id = "start"
+//         stopAndStart.disabled = false
+//         countTap += 1
+//         stopTimer.disabled = true
+//         startTimer.disabled = false
+//         console.log("countTap = " + countTap + "if")
+//     }
+//     else {
+//         intervalId = setInterval(timeUpdate,1000)
+//         stopAndStart.id = "stopAndStart"
+//         countTap += 1
+//         console.log("countTap = " + countTap + "else")
+//         console.log(intervalId + "else")
+//         startTimer.disabled = true
+//         stopTimer.disabled = false
+//     }
+// }
+
+const data = {
+    id: 1,
+    text: "it`s text",
+    status:true,
 }
-
-let intervalId = setInterval(timeUpdate,1000)
-
-
-
-startTimer.addEventListener("click", e => {
-    stopAndStart.disabled = false
-    stopTimer.disabled = false
-    startTimer.disabled = true
-    intervalId = setInterval(timeUpdate,1000)
-    countTap += 1
-    console.log("countTap = " + countTap + "startTimer")
-    console.log(intervalId + "intervalId")
-})
-stopTimer.addEventListener("click", e =>{
-    stopAndStart.disabled = true
-    stopTimer.disabled = true
-    startTimer.disabled = false
-    countTap += 1
-    clearInterval(intervalId)
-    console.log("countTap = " + countTap + "stopTimer")
-    console.log(intervalId + "intervalId")
-
-})
-
-stopAndStart.addEventListener("click", oneClickFun)
-
-function oneClickFun() {
-    if(countTap % 2 === 0) {
-        clearInterval(intervalId)
-        stopAndStart.id = "start"
-        stopAndStart.disabled = false
-        countTap += 1
-        stopTimer.disabled = true
-        startTimer.disabled = false
-        console.log("countTap = " + countTap + "if")
+console.log("Promise go to back")
+const promise = new Promise((resolve, rejects) => {
+    setTimeout(() => {
+        if (data.id) resolve(data),console.log("Promise launch")
+    rejects(data)
+    })
+},2000)
+    .then(data => {
+        setTimeout(() => {
+            if (data.id = "New id") {
+        data.id = " this id it`s Empty"
     }
-    else {
-        intervalId = setInterval(timeUpdate,1000)
-        stopAndStart.id = "stopAndStart"
-        countTap += 1
-        console.log("countTap = " + countTap + "else")
-        console.log(intervalId + "else")
-        startTimer.disabled = true
-        stopTimer.disabled = false
-    }
-}
+    console.log(data)
+    return data
+},2000)
+    }).catch(data => {
+        setTimeout(() => {
+        data.id = "New id"
+        console.log("If Error")
+        console.log(data)
+        return data
+    },2000)
+})
+
+
+    
